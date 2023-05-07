8d294c6e779dcb366d26114e8daddf8ec1057586
cucker0 <hanxiao2100@qq.com>
2023-05-07 18:08:13 +0800

    add Dockerfile_2.2, use dump-init

---
+++ dns/pkg/linux/server-start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/dumb-init /bin/sh
+
+## launch a process in the background
+# start MySQL
+/usr/sbin/mysqld --user=mysql -D
+# start url-forwarder
+LOG_PATH=/data/logs/url-forwarder;
+/usr/bin/bash -c "if [ ! -d ${LOG_PATH} ]; then mkdir -p ${LOG_PATH}; fi; /bin/sleep 2";
+/usr/local/java/jdk/bin/java \
+  -server -Xms2g -Xmx2g -XX:+UseG1GC -verbose:gc \
+  -XX:+HeapDumpOnOutOfMemoryError \
+  -Duser.timezone=GMT+8 \
+  -Xlog:gc*=info:file=/data/logs/url-forwarder/url-forwarder-gc.log::filesize=50M,filecount=4 \
+  -XX:HeapDumpPath=/data/logs/url-forwarder \
+  -jar /data/software/url-forwarder/url-forwarder-0.0.1-SNAPSHOT.jar \
+  --spring.config.additional-location=/data/software/url-forwarder/application.yml &
+# start named (bind)
+/usr/local/bind/sbin/named -n 1 -u named -c /usr/local/bind/etc/named.conf
+
+## At the end, launch another process in the foreground
+# start BindUI
+/usr/local/python3.11.3/bin/python3 /data/webroot/BindUI/manage.py runserver 0.0.0.0:8000
