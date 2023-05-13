49ef11f8676574c0cd9eccfead09a1f861d7e00f
cucker0 <hanxiao2100@qq.com>
2023-05-13 11:34:06 +0800

    update dns dockerfile

--- dns/pkg/MySQL/update_root_password.sh
+++ dns/pkg/MySQL/update_root_password.sh
@@ -4,7 +4,10 @@ NEW_PASSWORD='Py123456!'
 INIT_PASSWORD=$(grep "root@localhost" /var/log/mysqld.log |awk -F "root@localhost: " '{print $2}')
 
 main() {
+    # 修改 "root@localhost" 用户密码
     mysql -uroot -p${INIT_PASSWORD} --connect-expired-password < /pkg/MySQL/alter_user_password.sql;
+    # 或这样修改 root@localhost 密码
+    #mysql -uroot -p${INIT_PASSWORD} --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Py123456!' ;"
     mysql -uroot -p${NEW_PASSWORD} < /pkg/MySQL/db.sql    
 }
 
