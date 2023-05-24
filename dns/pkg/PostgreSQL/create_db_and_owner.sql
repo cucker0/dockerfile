d134ee1b44fcbd0db880c6b0f32519cdf02f0f56
cucker0 <hanxiao2100@qq.com>
2023-05-24 20:31:14 +0800

    update dns/Dockerfile_all_1.0

--- dns/pkg/PostgreSQL/create_db_and_owner.sql
+++ dns/pkg/PostgreSQL/create_db_and_owner.sql
@@ -1,4 +1,4 @@
 -- sudo -u postgres psql -d testdb -U postgres -f /path/xxx.sql
 
 CREATE USER dns_wr WITH ENCRYPTED PASSWORD 'Ww123456!';
-CREATE DATABASE dns OWNER dns_wr ENCODING=UTF8;
+CREATE DATABASE dns WITH ENCODING = 'UTF-8' TEMPLATE = template0 OWNER dns_wr;
